e26ae02d0f3d7ff0894e8b9a4ab03032985d8f89
David Siaw <davidsiaw@gmail.com>
2020-06-24 23:57:47 +0900

    build doc

--- deploy-doc.bash
+++ deploy-doc.bash
@@ -7,19 +7,9 @@ fi
 
 cd docs
 
-git clone git@github.com:davidsiaw/tataru.git build
-cd build
-  git checkout gh-pages
-cd ..
-
-cp -r build/.git ./gittemp
 bundle exec weaver build -r https://tataru.astrobunny.net
-cp -r ./gittemp build/.git
+
 pushd build
 echo tataru.astrobunny.net > CNAME
 cp 404/index.html 404.html
-git add .
-git add -u
-git commit -m "update `date`"
-ssh-agent bash -c 'ssh-add ~/.ssh/id_github.com; git push -u origin gh-pages'
 popd
